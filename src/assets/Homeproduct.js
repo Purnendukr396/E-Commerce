39f21eecebd52d97c5190fb93c43ac1ccb6e380e
Purnendu396 <purnendukr650@gmail.com>
2026-06-25 00:36:21 +0530

    Add Latest  products to Home Page

--- src/assets/Homeproduct.js
+++ src/assets/Homeproduct.js
@@ -1,6 +1,13 @@
 import productimag from "./p_img2.png";
 import trouser from "./p_img10.png";
 import top from "./p_img17.png";
+import cottontop from "./p_img29.png";
+import p_img25 from './p_img25.png';
+import p_img30 from './p_img30.png'
+import p_img14 from './p_img14.png';
+import p_img36 from './p_img36.png';
+import p_img21 from './p_img21.png';
+
 
 export const products = [
   {
@@ -24,4 +31,44 @@ export const products = [
     image:top,
     description:"A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
   },
+  {
+    id:4,
+    name:"Women Round Neck Cotton Top",
+    price:"$60",
+    image:cottontop,
+    description:"A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+  },
+  {
+          id: 5,
+          name: "Girls Round Neck Cotton Top",
+          description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+          price:"$45",
+          image: p_img30,
+  },
+  {
+          id: 6,
+          name: "Boy Round Neck Pure Cotton T-shirt",
+          description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+          price:"$82",
+          image:p_img14,
+          
+      },
+
+         
+
+   {
+        id: 7,
+        name: "Women Zip-Front Relaxed Fit Jacket",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+        price:"$72",
+        image: p_img21,
+       
+    },
+    {
+        _id: 8,
+        name: "Women Zip-Front Relaxed Fit Jacket",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+        price: "$27",
+        image: p_img36,
+    },
 ];
